a412e3978d62a0dda11f099cf3c489fd6ef18ae8
vick7y <69018311+vick7y@users.noreply.github.com>
2020-08-17 15:50:15 -0500

    Add files via upload

---
+++ Capstone Project Kickoff.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Notebook for Coursera Capstone Project
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print('Hello Capstone Project Course!')
+
